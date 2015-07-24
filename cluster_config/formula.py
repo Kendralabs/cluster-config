a4e7a6dc974d0c2172b59c187cd24136c022e865
ashahba <Abolfazl.Shahbazi@intel.com>
2015-07-23 20:29:55 -0700

    Added some new params to formula-args.yaml file and also some error checking

--- cluster_config/formula.py
+++ cluster_config/formula.py
@@ -1,15 +1,43 @@
-from cluster_config import log
-import math
+# from cluster_config import log
 
 # Simple MB, GB and TB to Bytes calculator
-log.info(args['NUM_THREADS'])
 KiB = 1024
 MiB = KiB * 1024
 GiB = MiB * 1024
 TiB = GiB * 1024
+MIN_NM_MEMORY = 8 * GiB
 
-cdh = {}
-atk = {}
+# Validate user defined parameters in forumual-args.yaml file
+NUM_THREADS = args['NUM_THREADS']
+if (NUM_THREADS < 1):
+    log.fatal("{0} must be at least 1".format("NUM_THREADS"))
+
+OVER_COMMIT_FACTOR = args['OVER_COMMIT_FACTOR']
+if (OVER_COMMIT_FACTOR < 1):
+    log.fatal("{0} must be at least 1".format("OVER_COMMIT_FACTOR"))
+
+MEM_FRACTION_FOR_HBASE = args['MEM_FRACTION_FOR_HBASE']
+if (MEM_FRACTION_FOR_HBASE < 0 or MEM_FRACTION_FOR_HBASE >= 1):
+    log.fatal("{0} must be non-nagative and smaller than 1".format("MEM_FRACTION_FOR_HBASE"))
+
+MEM_FRACTION_FOR_OTHER_SERVICES = args['MEM_FRACTION_FOR_OTHER_SERVICES']
+if (MEM_FRACTION_FOR_OTHER_SERVICES < 0 or (MEM_FRACTION_FOR_OTHER_SERVICES >= (1 - MEM_FRACTION_FOR_HBASE))):
+    log.fatal("{0} must be non-nagative and smaller than {1}".format("MEM_FRACTION_FOR_OTHER_SERVICES",
+                                                                     1 - MEM_FRACTION_FOR_HBASE))
+
+MAPREDUCE_JOB_COUNTERS_MAX = args['MAPREDUCE_JOB_COUNTERS_MAX']
+if (MAPREDUCE_JOB_COUNTERS_MAX < 120):
+    log.fatal("{0} must be greater or equal {1}".format("MAPREDUCE_JOB_COUNTERS_MAX", 120))
+
+SPARK_DRIVER_MAXPERMSIZE = args['SPARK_DRIVER_MAXPERMSIZE']
+if (SPARK_DRIVER_MAXPERMSIZE < 512):
+    log.fatal("{0} must be at least {1}MB.".format("SPARK_DRIVER_MAXPERMSIZE", 512))
+
+MAPREDUCE_MAP_MINIMUM_MEMORY_MB = args['MAPREDUCE_MAP_MINIMUM_MEMORY_MB']
+if (MAPREDUCE_MAP_MINIMUM_MEMORY_MB < 512):
+    log.fatal("Containers less than {0}MB are not supported".format(MAPREDUCE_MAP_MINIMUM_MEMORY_MB))
+elif (MAPREDUCE_MAP_MINIMUM_MEMORY_MB != (MAPREDUCE_MAP_MINIMUM_MEMORY_MB / 512) * 512):
+    log.fatal("{0} must be dividable by {1}".format('MAPREDUCE_MAP_MINIMUM_MEMORY_MB', 512))
 
 hosts = cluster.yarn.nodemanager.hosts.all()
 max_cores = 0
@@ -20,19 +48,24 @@ for key in hosts:
     if hosts[key].totalPhysMemBytes > max_memory:
         max_memory = hosts[key].totalPhysMemBytes
 
+cdh = {}
+atk = {}
+
 ###### These values are gathered by the tool from Cluster ######
 NUM_NM_WORKERS = len(hosts)
 NM_WORKER_CORES = max_cores
 NM_WOKER_MEM = max_memory
 
-NUM_THREADS = 1  # This should be set to the maximum number of munti-tanent users
-OVER_COMMIT_FACTOR = 1.30
-MAX_JVM_MEMORY = 32768 * MiB
-MEM_FRACTION_FOR_HBASE = 0.20
-MEM_FRACTION_FOR_OTHER_SERVICES = 0.20
-MAPREDUCE_JOB_COUNTERS_MAX = 500
+if (max_memory < (MIN_NM_MEMORY)):
+    log.fatal("Running the toolkit with less than {0}GB memory for YARN is not supported.".format(MIN_NM_MEMORY))
+elif (max_memory <= (256 * GiB)):
+    MAX_JVM_MEMORY = max_memory / 4  # Java Heap Size should not go over 25% of total memory per node manager
+else:
+    MAX_JVM_MEMORY = 64 * GiB  # for node managers with greater than 256 GB RAM, JVM memory should still be at most 64GB
 
-SPARK_DRIVER_MAXPERMSIZE = 512
+if ((MAX_JVM_MEMORY / MiB) < MAPREDUCE_MAP_MINIMUM_MEMORY_MB):
+    log.fatal("Container larger than {0}MB are not supported".format(MAX_JVM_MEMORY))
+
 atk["intel.taproot.analytics.engine.spark.conf.properties.spark.driver.maxPermSize"] = \
     "\"%dm\"" % (SPARK_DRIVER_MAXPERMSIZE)
 
@@ -64,10 +97,6 @@ cdh["HBASE.REGIONSERVER.REGIONSERVER_BASE.HBASE_REGIONSERVER_JAVA_HEAPSIZE"] = \
     int(MEM_FOR_HBASE_REGION_SERVERS / OVER_COMMIT_FACTOR)
 
 cdh["YARN.RESOURCEMANAGER.RESOURCEMANAGER_BASE.YARN_SCHEDULER_INCREMENT_ALLOCATION_MB"] = 512
-
-# The recommended value for this is less or equal 32GB
-MAPREDUCE_MAP_MINIMUM_MEMORY_MB = \
-    cdh["YARN.RESOURCEMANAGER.RESOURCEMANAGER_BASE.YARN_SCHEDULER_INCREMENT_ALLOCATION_MB"] * 2
 
 cdh["YARN.RESOURCEMANAGER.RESOURCEMANAGER_BASE.YARN_SCHEDULER_MAXIMUM_ALLOCATION_MB"] = \
     (
@@ -140,6 +169,8 @@ if NUM_THREADS > (CONTAINERS_ACCROSS_CLUSTER / 2):
               .format((min(CONTAINERS_ACCROSS_CLUSTER, CONTAINERS_ACCROSS_CLUSTER) / 2))
     )
 
+log.info("{0} could be as large as {1} for multi-tenacty".format("NUM_THREADS", (CONTAINERS_ACCROSS_CLUSTER / 2)))
+
 atk["intel.taproot.analytics.engine.spark.conf.properties.spark.yarn.numExecutors"] = \
     int((CONTAINERS_ACCROSS_CLUSTER - NUM_THREADS) / NUM_THREADS)
 
@@ -161,7 +192,4 @@ atk["intel.taproot.analytics.api.giraph.giraph.maxWorkers"] = \
 
 atk["intel.taproot.analytics.api.giraph.mapreduce.map.java.opts.max.heap"] = \
     "\"-Xmx%sm\"" % (cdh["YARN.GATEWAY.GATEWAY_BASE.MAPREDUCE_MAP_JAVA_OPTS_MAX_HEAP"] / MiB)
-
-
-
 
