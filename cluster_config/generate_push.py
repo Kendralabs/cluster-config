5d308a3e3e42f9c2528b65c509344528e9e8956d
ashahba <Abolfazl.Shahbazi@intel.com>
2015-08-20 18:09:51 -0700

    Fix a minor issue with CDH restart and cleaning the snapshot folders name

--- cluster_config/generate_push.py
+++ cluster_config/generate_push.py
@@ -1,31 +1,31 @@
 from __future__ import print_function
 import argparse
 from cluster_config.cdh.cluster import Cluster
-from cluster_config import auto_config, config
+from cluster_config import generate, push
 
 
 def cli(parser=None):
     if parser is None:
         parser = argparse.ArgumentParser(description="Auto generate various CDH configurations based on system resources")
 
-    parser = auto_config.cli(parser)
-    parser = config.cli(parser)
+    parser = generate.cli(parser)
+    parser = push.cli(parser)
 
     return parser
 
 
 def main():
     from cluster_config.cdh.cluster import Cluster
-    from cluster_config import push
+    #from cluster_config import push
     from cluster_config.cli import parse
-    args = parse(push.cli())
+    args = parse(cli())
     cluster = Cluster(args.host, args.port, args.username, args.password, args.cluster)
-    push.run(args, cluster)
+    run(args, cluster)
 
 
 def run(args, cluster=None):
     if cluster is None:
         cluster = Cluster(args.host, args.port, args.username, args.password, args.cluster)
 
-    auto_config.run(args, cluster)
-    config.run(args, cluster)
+    generate.run(args, cluster)
+    push.run(args, cluster)
